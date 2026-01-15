34dbfb421ad30c892e77694a01e1395817a185a4
Logan Williams <loftylogan@gmail.com>
2026-01-15 16:32:47 -0600

    Frontend scroll fix + icon

--- src/components/PackageBrowser.tsx
+++ src/components/PackageBrowser.tsx
@@ -16,12 +16,11 @@ export default function PackageBrowser( {packages} : PackageBrowserProps)
     const [selectedPackage, setSelectedPackage] = useState<PackageRoot | null>(null)
     const [searchField, setSearchField] = useState("")
 
-    console.log(selectedPackage)
     return (
         <div className='h-full'>
             <div className="bg-white h-fit border-b border-default">
                 <div className="w-screen flex flex-wrap items-center justify-left mx-1 p-4">
-                    <img src="/favicon.ico" className="aspect-ratio-1/1 w-15 mx-8"/>
+                    <img src="/favicon.png" className="aspect-ratio-1/1 w-15 mx-8"/>
                     <input onChange={(e) => setSearchField(e.target.value)} id="search" className="h-10 w-[100% - 40px] grow bg-gray-100 p-3 rounded-xl mx-5" placeholder="Search Packages..."></input>
                 </div>
             </div>
@@ -48,7 +47,7 @@ export default function PackageBrowser( {packages} : PackageBrowserProps)
                 </div>
                 <div className="col-span-11 bg-gray-300 w-full overscroll-none overflow-y-scroll overflow-x-hidden [scrollbar-width:none]">
                     {selectedPackage != null && 
-                    <div className="flex flex-col divide-y divide-gray-200 m-5 bg-white">
+                    <div className="flex flex-col divide-y divide-gray-200 m-5 mb-30 bg-white">
                         <div className="text-5xl p-5">{selectedPackage?.name}</div>
                             {selectedPackage?.versions[Object.keys(selectedPackage?.versions!).at(-1) as string].description != null && 
                                 <div className="p-5">
